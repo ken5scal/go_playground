5d38649d2e480daa1f49870d817edc3fbbb0297f
ken5scal <kengoscal@gmail.com>
2016-10-23 11:37:50 +0900

    Add method with pointer receivers

--- main.go
+++ main.go
@@ -21,11 +21,16 @@ func Abs(v Vertex) float64 {
 	return math.Sqrt(v.X * v.X + v.Y * v.Y)
 }
 
+func (v *Vertex) Scale(f float64) {
+	v.X = v.X * f
+	v.Y = v.Y * f
+}
+
 type MyFloat float64
 // You can only declare a mehtod with a receiver
 // whose type is defined in hte same package as the method
 func (f MyFloat) Abs() float64 {
-	if f<0 {
+	if f < 0 {
 		return float64(-f)
 	}
 	return float64(f)
@@ -35,6 +40,8 @@ func main() {
 	v := Vertex{X:3, Y:4}
 	fmt.Println(v.Abs())
 	fmt.Println(Abs(v))
+	v.Scale(10)
+	fmt.Println(v.Abs())
 
 	f := MyFloat(-math.Sqrt2)
 	fmt.Println(f.Abs())
