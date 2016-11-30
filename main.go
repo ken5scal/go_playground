d3b86d88653de51b72ebc3563e80ff0d797d2273
ken5scal <kengoscal@gmail.com>
2016-11-30 09:09:36 +0900

    Upload

--- main.go
+++ main.go
@@ -245,6 +245,8 @@ func main() {
 	}
 	time.Sleep(time.Millisecond)
 	fmt.Println(c2.Value("hoge"))
+
+	ExamplePingPong()
 }
 
 type SafeCounter struct {
@@ -360,6 +362,46 @@ func (r MyReader) Read(b []byte) (int, error) {
 type PingPongPayload struct {
 	Counter int
 }
-func ExamplePingPong() {
 
+func ExamplePingPong() {
+	var p PingPongPayload
+	chA := make(chan *PingPongPayload)
+	chB := make(chan *PingPongPayload)
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		for {
+			p, ok := <-chA; if !ok {
+				break
+			}
+			fmt.Printf("chA: p.Counter = %v\n", p.Counter)
+			p.Counter ++
+			if p.Counter >6 {
+				break
+			}
+			chB<-p
+		}
+		close(chB)
+	}()
+
+	go func() {
+		defer wg.Done()
+		for {
+			p, ok:=<-chB ; if !ok {
+				break
+			}
+			fmt.Printf("chB: p.Counter =%v\n", p.Counter)
+			p.Counter++
+			if p.Counter > 6 {
+				break
+			}
+			chA<-p
+		}
+		close(chA)
+	}()
+	chA <- &p
+	wg.Wait()
 }
