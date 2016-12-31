b57ed4860caa027911d0bfe73864c4d82faaefda
ken5scal <kengoscal@gmail.com>
2016-12-31 21:15:15 +0900

    Rrefactor

--- callback/main.go
+++ callback/main.go
@@ -20,7 +20,7 @@ func visit(numbers []int, callback func(int)) {
 }
 
 func filter(numbers []int, callback func(int) bool) [] int {
-	xs := []int{}
+	var xs []int
 	for _, n := range numbers {
 		if callback(n) {
 			xs = append(xs, n)
