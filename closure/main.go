7ba7abc7937bab8544c16d66d0d2205a94f039e4
ken5scal <kengoscal@gmail.com>
2016-12-31 21:03:46 +0900

    Add closure package

--- closure/main.go
+++ closure/main.go
@@ -3,17 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	x := 0
-	increment := func() int {
-		x++
-		return x
-	}
+	//2
+	//var x int <- no longer package level scope
+	//increment := func() int {  <- anonymous function
+	//	x++
+	//	return x
+	//}
+	increment := wrapper()
 	fmt.Println(increment())
 	fmt.Println(increment())
 }
 
+func wrapper() func() int {
+	var x int
+	return func() int {
+		x++
+		return x
+	}
+}
+
 // 1
-// var x int
+// var x int <- package level scope
 //func increment() int {
 //	x++
 //	return x
