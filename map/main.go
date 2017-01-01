d2b0227de83899c15655cdbb95e84ec6ba681c49
ken5scal <kengoscal@gmail.com>
2017-01-01 16:33:52 +0900

    Creating Hash table

--- map/main.go
+++ map/main.go
@@ -1,11 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"log"
+	"bufio"
+	"os"
+)
 
 func main() {
 	// reference type. pointing to actual data type
 	//
-	m := make(map[string ]int)
+	m := make(map[string]int)
 	m["k1"] = 7
 	m["k2"] = 13
 
@@ -23,4 +29,33 @@ func main() {
 	myGreeting["A"] = "Good morning."
 	myGreeting["Jenny"] = "Bonjour."
 	fmt.Println(myGreeting)
+
+	//	HASH
+	res, err := http.Get("http://www-01.sil.org/linguistics/wordlists/english/wordlist/wordsEn.txt")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	words := make(map[string]string)
+	sc := bufio.NewScanner(res.Body)
+	sc.Split(bufio.ScanWords)
+	for sc.Scan() {
+		words[sc.Text()] = ""
+	}
+
+	if err := sc.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "reading input:", err)
+	}
+
+	i := 0
+	for k, _ := range words {
+		fmt.Println(k)
+		if i == 200 {
+			break
+		}
+		i++
+	}
+
+	//str := string(bs)
+	//fmt.Println(str)
 }
