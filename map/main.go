f2e3760e643bcb19d21623083552ab1c0a542351
ken5scal <kengoscal@gmail.com>
2017-01-01 16:14:21 +0900

    Skip map exerceise

--- map/main.go
+++ map/main.go
@@ -1,4 +1,26 @@
 package main
 
+import "fmt"
+
 func main() {
+	// reference type. pointing to actual data type
+	//
+	m := make(map[string ]int)
+	m["k1"] = 7
+	m["k2"] = 13
+
+	fmt.Println("map:", m)
+	delete(m, "k2")
+	fmt.Println("map:", m)
+
+	v, ok := m["k1"]
+	fmt.Println("ok?:", ok, v)
+
+	var n = map[string]int{"foo" : 1, "bar":2}
+	fmt.Println("map", n)
+
+	var myGreeting = make(map[string]string)
+	myGreeting["A"] = "Good morning."
+	myGreeting["Jenny"] = "Bonjour."
+	fmt.Println(myGreeting)
 }
