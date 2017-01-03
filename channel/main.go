bb4c763654164311342ccd2ca4bd8bb015871489
ken5scal <kengoscal@gmail.com>
2017-01-03 12:31:19 +0900

    First channel

--- channel/main.go
+++ channel/main.go
@@ -1,4 +1,72 @@
 package main
 
+import (
+	"fmt"
+	"time"
+	"sync"
+)
+
 func main() {
+	// Do not communicate by sharing memory (Dont block by mutex or atomic function when goroutine accesses to it),
+	// share memory by communication
+
+	// ANd this is what channel does
+	c := make(chan int) // Unbuffered channel
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			c <- i  // blocks until fmt.println(<-c) is executed
+		}
+	}()
+
+	go func() {
+		for {
+			fmt.Println(<-c)
+		}
+	}()
+
+	time.Sleep(time.Second) // Gives time to Execute all code
+
+	// ----- Better because it wont need additional goroutine, and sleep function"
+	//c2 := make(chan int)
+	//go func() {
+	//	for i := 0; i < 10; i++ {
+	//		c2 <- i
+	//		fmt.Printf("i: %v\n", i)
+	//	}
+	//	close(c2) // Channel is closed, you cannot put values to channel.
+	//}()
+	//
+	//for n := range c2 { // Wait until it receives.
+	//	fmt.Println(n)
+	//}
+
+	c3 := make(chan int)
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+		//wg.Add(1) // This is bad, because goroutine is trying to access shared variable
+		for i := 0; i < 10; i++ {
+			c3 <- i
+		}
+		wg.Done()
+	}()
+
+	go func() {
+		//wg.Add(1) // This is bad, because goroutine is trying to access shared variable
+		for i := 0; i < 10; i++ {
+			c3 <- i
+		}
+		wg.Done()
+	}()
+
+	go func() {
+		wg.Wait()
+		close(c3)
+	}()
+
+	for n := range c3 {
+		fmt.Println(n)
+	}
 }
