dff3e67e8a5f3d25afe783334eeba573ef922c74
ken5scal <kengoscal@gmail.com>
2017-01-04 08:57:59 +0900

    Add incrementor

--- channel/pass_return_channel/main.go
+++ channel/pass_return_channel/main.go
@@ -8,6 +8,12 @@ func main() {
 	for n := range cSum {
 		fmt.Println(n)
 	}
+
+	c1 := incrementor2("Foo:")
+	c2 := incrementor2("Bar:")
+	c3 := puller(c1)
+	c4 := puller(c2)
+	fmt.Println("Final Counter:", <-c3 + <-c4)
 }
 
 func incrementor() <-chan int {
@@ -15,6 +21,18 @@ func incrementor() <-chan int {
 	go func() {
 		for i := 0; i < 10; i++ {
 			out <- i
+		}
+		close(out)
+	}()
+	return out
+}
+
+func incrementor2(s string) <-chan int {
+	out := make(chan int)
+	go func() {
+		for i := 0; i < 20; i++ {
+			out <- 1
+			fmt.Println(s, i)
 		}
 		close(out)
 	}()
