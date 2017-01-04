0da398190e69d5e7fe87048c67c55f35cbf660c3
ken5scal <kengoscal@gmail.com>
2017-01-04 09:06:34 +0900

    Finish another dead lock problem

--- channel/deadlock-challenge/main.go
+++ channel/deadlock-challenge/main.go
@@ -8,4 +8,19 @@ func main() {
 	// Channels are the pipes that connect concurrent goroutines. You can send values into channels from one goroutine and receive those values into another goroutine.
 	go func() {c<-1}()
 	fmt.Println(<-c)
+
+	c1 := make(chan int)
+	go func () {
+		for i := 0; i < 10; i++ {
+			c1 <- i
+		}
+		// Without closing, it will deadlock
+		close(c1)
+	}()
+
+	// fmt.Println(i) This only print 0
+	//for { // This will be deadlock
+	for i := range c1{
+		fmt.Println(i)
+	}
 }
