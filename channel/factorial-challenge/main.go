8b2c848e4ef7d1b09de9942e22539e78e9500062
ken5scal <kengoscal@gmail.com>
2017-01-05 07:19:38 +0900

    Updte

--- channel/factorial-challenge/main.go
+++ channel/factorial-challenge/main.go
@@ -6,7 +6,11 @@ func main() {
 	f := factorial(4)
 	fmt.Println("Total:", f)
 
-	fmt.Println("Total: ", factorial2(4))
+	//fmt.Println("Total: ", factorial2(4))
+	c := factorial2(4)
+	for n:= range c {
+		fmt.Println("Total:", n)
+	}
 }
 
 func factorial(n int) int {
@@ -17,8 +21,13 @@ func factorial(n int) int {
 	return total
 }
 
-func factorial2(n int) int {
+
+// goroutine is helpful when you have many processing to do.
+// if you want to run factorial calculations for 1000 processes,
+// then you want to utilize all of the CPU core using goroutine.
+func factorial2(n int) chan int {
 	c := make(chan int)
+
 	//total := 1
 	//go func() {
 	//	for i := n; i > 0; i-- {
@@ -31,4 +40,14 @@ func factorial2(n int) int {
 	//	total *= i
 	//}
 	//return total
+
+	go func() {
+		total := 1
+		for i := n; i > 0; i-- {
+			total *= i
+		}
+		c <- total
+		close(c)
+	}()
+	return c
 }
