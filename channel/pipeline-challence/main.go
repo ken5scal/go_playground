cb7f238c7ad2365a3f5d2028be205262eabc6f8b
ken5scal <kengoscal@gmail.com>
2017-01-05 07:28:23 +0900

    more pipleine

--- channel/pipeline-challence/main.go
+++ channel/pipeline-challence/main.go
@@ -1,4 +1,38 @@
 package main
 
+import "fmt"
+
 func main() {
+	// Pipeline is a series of stages connected by channels where each stage is a group of gorouines running the same functionn
+	//c := gen(2,3)
+	//out := sq(c)
+	//
+	//fmt.Println(<-out) // 4
+	//fmt.Println(<-out) // 9
+
+	for n := range sq(sq(gen(2,3))) {
+		fmt.Println(n) // 16 , then 81
+	}
+}
+
+func gen(nums ...int) <-chan int {
+	out := make(chan int)
+	go func()   {
+		for _, n:= range nums{
+			out <- n
+		}
+		close(out)
+	}()
+	return out
+}
+
+func sq(in <-chan int) <-chan int {
+	out := make(chan int)
+	go func() {
+		for n:= range in{
+			out <- n*n
+		}
+		close(out)
+	}()
+	return out
 }
