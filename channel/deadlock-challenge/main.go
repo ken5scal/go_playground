728a3995a19186c591200f7984832082f6a792ef
ken5scal <kengoscal@gmail.com>
2017-01-04 09:02:02 +0900

    Fix problem

--- channel/deadlock-challenge/main.go
+++ channel/deadlock-challenge/main.go
@@ -1,4 +1,11 @@
 package main
 
+import "fmt"
+
 func main() {
+	c := make(chan int)
+	//c <- 1 Code Blocked here.
+	// Channels are the pipes that connect concurrent goroutines. You can send values into channels from one goroutine and receive those values into another goroutine.
+	go func() {c<-1}()
+	fmt.Println(<-c)
 }
