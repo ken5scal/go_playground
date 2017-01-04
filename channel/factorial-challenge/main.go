436c35215e08c0d29de8d701d18dd44834fde910
ken5scal <kengoscal@gmail.com>
2017-01-05 06:57:39 +0900

    Factorial in usual way

--- channel/factorial-challenge/main.go
+++ channel/factorial-challenge/main.go
@@ -1,4 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
+	f := factorial(4)
+	fmt.Println("Total:", f)
+}
+
+func factorial(n int) interface{} {
+	total := 1
+	for i := n; i > 0; i-- {
+		total *= i
+	}
+	return total
 }
