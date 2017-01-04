73ce81625146311e92094816b9263c9dfaafe0a3
ken5scal <kengoscal@gmail.com>
2017-01-05 07:13:39 +0900

    Factorial in channel

--- channel/factorial-challenge/main.go
+++ channel/factorial-challenge/main.go
@@ -5,11 +5,29 @@ import "fmt"
 func main() {
 	f := factorial(4)
 	fmt.Println("Total:", f)
+
+	fmt.Println("Total: ", factorial2(4))
 }
 
-func factorial(n int) interface{} {
+func factorial(n int) int {
 	total := 1
 	for i := n; i > 0; i-- {
+		total *= i
+	}
+	return total
+}
+
+func factorial2(n int) int {
+	c := make(chan int)
+	total := 1
+	go func() {
+		for i := n; i > 0; i-- {
+			c<-i
+		}
+		close(c)
+	}()
+
+	for i := range c {
 		total *= i
 	}
 	return total
