3cb2f485d60bd9a484bb31a215c9123ae782ae20
ken5scal <kengoscal@gmail.com>
2017-01-05 07:36:23 +0900

    Start implementing new pipeline challenge

---
+++ channel/pipeline-challence/factorial-challenge/main.go
@@ -0,0 +1,70 @@
+package main
+
+import "fmt"
+
+func main() {
+	f := factorialNormal(4)
+	fmt.Println("Total:", f)
+
+	//fmt.Println("Total: ", factorialChannel(4))
+	c := factorialChannel(4)
+	for n:= range c {
+		fmt.Println("Total:", n)
+	}
+
+
+	in := gen()
+	f2 := factorialPipeline(in)
+	for n := range f2 {
+		fmt.Println("Total:", n)
+	}
+}
+
+func gen() <-chan int {
+	out := make(chan int)
+	return out
+}
+
+func factorialPipeline(ints <-chan int) <-chan int {
+	out := make(chan int)
+	return out
+}
+
+func factorialNormal(n int) int {
+	total := 1
+	for i := n; i > 0; i-- {
+		total *= i
+	}
+	return total
+}
+
+
+// goroutine is helpful when you have many processing to do.
+// if you want to run factorialNormal calculations for 1000 processes,
+// then you want to utilize all of the CPU core using goroutine.
+func factorialChannel(n int) chan int {
+	c := make(chan int)
+
+	//total := 1
+	//go func() {
+	//	for i := n; i > 0; i-- {
+	//		c<-i
+	//	}
+	//	close(c)
+	//}()
+	//
+	//for i := range c {
+	//	total *= i
+	//}
+	//return total
+
+	go func() {
+		total := 1
+		for i := n; i > 0; i-- {
+			total *= i
+		}
+		c <- total
+		close(c)
+	}()
+	return c
+}
