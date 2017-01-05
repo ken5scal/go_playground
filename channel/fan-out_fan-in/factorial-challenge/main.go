f2e9339b5ba5059fef95a70e68cb1cd12b3908dd
ken5scal <kengoscal@gmail.com>
2017-01-06 08:46:26 +0900

    FInish implementing fan-out_fan_in factorial

--- channel/fan-out_fan-in/factorial-challenge/main.go
+++ channel/fan-out_fan-in/factorial-challenge/main.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+)
 
 /*
 Challenge #1:
@@ -8,6 +12,70 @@ Challenge #1:
 
 Challenge #2:
 -- While running the factorial computations, try to find how much of your resources are being used.
- */
+*/
 func main() {
+	in := gen()
+	c1 := factorial(in) //<- worker
+	c2 := factorial(in) //<- worker
+	c3 := factorial(in) //<- worker
+	for n := range merge(c1, c2, c3) {
+		fmt.Println(n)
+	}
+}
+
+func gen() chan int {
+	out := make(chan int)
+	go func() {
+		for i := 0; i < 10; i++ {
+			for j := 3; j < 13; j++ {
+				out <- j
+			}
+		}
+		close(out)
+	}()
+	return out
+}
+
+func factorial(nums chan int) chan int {
+	out := make(chan int)
+
+	go func() {
+		for num := range nums {
+			out <- func(n int) int {
+				total := 1
+				for i := n; i > 0; i-- {
+					total *= i
+				}
+				return total
+			}(num)
+		}
+		close(out)
+	}()
+
+	return out
+}
+
+func merge(cs ...chan int) chan int {
+	out := make(chan int)
+
+	var wg sync.WaitGroup
+	wg.Add(len(cs))
+
+	for _, c := range cs {
+		go func(ch chan int) {
+			for n := range ch {
+				out <- n
+			}
+			wg.Done()
+		}(c)
+	}
+
+	// Close out once all the output goroutine are done.
+	// This must start after the wg.Add Call
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+
+	return out
 }
